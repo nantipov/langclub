7b4e9fe0025393bd95ecf519888dcfb491842347
Nikolay Antipov <nick.antipov@gmail.com>
2015-09-16 13:13:55 +0300

    JSON tuning, logging tuning, initializer tuning

--- langclub-service/src/main/java/org/langclub/service/startup/ApplicationInitializer.java
+++ langclub-service/src/main/java/org/langclub/service/startup/ApplicationInitializer.java
@@ -1,9 +1,7 @@
 package org.langclub.service.startup;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -33,10 +31,9 @@ public class ApplicationInitializer implements WebApplicationInitializer {
         context.register(SpringApplicationConfiguration.class);
 
         // lifecycle
-//        servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, context);
         servletContext.addListener(new ContextLoaderListener(context));
 
-        // web context - empty one since no Spring MVC in service component
+        // web context
         AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
         ServletRegistration.Dynamic dispatcher =
                 servletContext.addServlet("dispatcher", new DispatcherServlet(webContext));
